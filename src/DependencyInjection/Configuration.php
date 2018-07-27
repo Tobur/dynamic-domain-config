cbd2ffc22520d55f2fecff3d6043196f9b08c6ed
Alexandr Sharamko <sharamko11@yandex.ru>
2018-07-27 15:09:17 +0300

    Fixed namespace

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SonataGenerator\DependencyInjection;
+namespace DynamicDomainConfig\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
